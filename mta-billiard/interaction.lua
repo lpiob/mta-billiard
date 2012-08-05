48037cae621bb6de1a3ad89a428c74bcf639319e
qaisjp <qaisjp@gmail.com>
2012-08-05 03:23:22 -0700

    More efficient code
    Improved the code efficiency by eliminating the if checks. The edit also prevents errors from occurring (whilst masking a coding mistake, which isn't quite right)
    
    Also converted polish word > "players"

--- mta-billiard/interaction.lua
+++ mta-billiard/interaction.lua
@@ -1,11 +1,6 @@
 addEvent("setPedAnimation", true)
 addEventHandler("setPedAnimation", root, function(block,anim,time,loop,updatePosition,interruptable, freezeLastFrame)
-
-	if (time==nil) then time=-1 end
-	if (loop==nil) then loop=true end
-	if (updatePosition==nil) then updatePosition=true end
-	if (interruptable==nil) then interruptable=true end
-	if (freezeLastFrame==nil) then freezeLastFrame=true end
+	time, loop, updatePosition, interruptable, freezeLastFrame = time or -1, loop and true or false, updatePosition and true or false, interruptable and true or false, freezeLastFrame and true or false
 --	bool setPedAnimation ( ped thePed [, string block=nil, string anim=nil, int time=-1, bool loop=true, bool updatePosition=true, bool interruptable=true, bool freezeLastFrame = true] )
 	setPedAnimation(source, block, anim, time, loop, updatePosition, interruptable, freezeLastFrame)
 
@@ -18,8 +13,8 @@ addEventHandler("broadcastCaptionedEvent", root, function(descr, lifetime, range
 
     local x,y,z=getElementPosition(source)
     local area=createColSphere(x,y,z,range)
-    local gracze=getElementsWithinColShape(area, "player")
-    for i,v in ipairs(gracze) do
+    local players=getElementsWithinColShape(area, "player")
+    for i,v in ipairs(players) do
 	if ((includesource or (source~=v)) and getElementInterior(v)==getElementInterior(source) and getElementDimension(v)==getElementDimension(source)) then
 	    outputChatBox("* " .. descr, v)
 	end
